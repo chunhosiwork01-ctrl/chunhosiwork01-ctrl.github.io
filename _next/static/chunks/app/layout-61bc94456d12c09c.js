f6e37bdcc0edfbdf21cd3f2457ee417395def79a
chunhosiwork01-ctrl <chunhosiwork01@gmail.com>
2026-08-01 00:06:45 +0800

    Add interactive portfolio page

---
+++ _next/static/chunks/app/layout-61bc94456d12c09c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2765:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2936,23))},2936:()=>{}},e=>{e.O(0,[5,937,770,358],()=>e(e.s=2765)),_N_E=e.O()}]);
